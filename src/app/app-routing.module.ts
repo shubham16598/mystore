e3aa4e50d623d4ac7eb544d2c9da1e2f2922c551
Shubham Singh Kalyanwat <shubham16598@gmail.com>
2021-06-06 11:49:17 +0530

    Initital project structure and routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/product-list', pathMatch: 'full'}, //redirect to prodct-list
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'product-detail', component: ProductItemDetailComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
